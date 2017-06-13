6b6797e024a7fbb482d72904f473005d19773781
etienne pelletier gagne <etienne.pelletier.gagne0@gmail.com>
2017-06-13 15:05:06 +0800

    done

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,49 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+  @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
+
+
+  private
+
+  def task_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:task).permit(:name, :description)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,37 @@
 Rails.application.routes.draw do
+
+  # get 'index', to: 'tasks#index'
+
+  # get 'tasks/:id', to: 'tasks#show'
+
+  # post 'tasks/new', to: 'tasks#new'
+
+  # get 'create', to: 'tasks#create'
+
+  # get 'edit', to: 'tasks#edit'
+
+  # get 'update', to: 'tasks#update'
+
+  # get 'destroy', to: 'tasks#destroy'
+
+
+  resources :tasks
+
+  # get 'tasks/index'
+
+  # get 'tasks/show'
+
+  # get 'tasks/new'
+
+  # get 'tasks/create'
+
+  # get 'tasks/edit'
+
+  # get 'tasks/update'
+
+  # get 'tasks/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
